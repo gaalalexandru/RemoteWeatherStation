c08728df7edad9d94019577dc80d058b574a14f1
Gaal Alexandru <alexandru.gaal-ext@continental-corporation.com>
2018-08-04 21:13:38 +0300

    1

--- RWS/Debug/makedep.mk
+++ RWS/Debug/makedep.mk
@@ -2,7 +2,11 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+bme280.c
+
 main.c
 
 spi_handler.c
+
+uart_handler.c
 
